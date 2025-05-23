d3bd57e5634d4c405b606d50cd4f603a132748d2
fbansept <bansept.franck@gmail.com>
2025-05-23 16:59:23 +0200

    jwt to fix

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
+const jwtUtils = require("jsonwebtoken");
 
 const app = express();
 
@@ -45,9 +47,97 @@ app.get("/produits/liste", (requete, resultat) => {
 app.post("/produit", (requete, resultat) => {
   const produit = requete.body;
 
-  console.log(produit);
+  //validation
+  if (
+    produit.nom == null ||
+    produit.nom == "" ||
+    produit.nom.length > 20 ||
+    produit.description.length > 50
+  ) {
+    return resultat.sendStatus(400); //bad request
+  }
 
-  resultat.statusCode(201).json(produit);
+  //verification si le nom du produit existe déjà
+  connection.query(
+    "SELECT * FROM produit WHERE nom = ?",
+    [produit.nom],
+    (err, lignes) => {
+      if (lignes.length > 0) {
+        return resultat.sendStatus(409); //conflict
+      }
+
+      connection.query(
+        "INSERT INTO produit (nom, description) VALUES (?, ?)",
+        [produit.nom, produit.description],
+        (err, lignes) => {
+          if (err) {
+            console.error(err);
+            return resultat.sendStatus(500); //internal server error
+          }
+
+          resultat.status(201).json(produit); //created
+        }
+      );
+    }
+  );
+});
+
+app.post("/inscription", (requete, resultat) => {
+  const utilisateur = requete.body;
+
+  const passwordHash = bcrypt.hashSync(utilisateur.password, 10);
+
+  connection.query(
+    "INSERT INTO utilisateur(email, password) VALUES (? , ?)",
+    [utilisateur.email, passwordHash],
+    (err, retour) => {
+      if (err && err.code == "ER_DUP_ENTRY") {
+        return resultat.sendStatus(409); //conflict
+      }
+
+      if (err) {
+        console.error(err);
+        return resultat.sendStatus(500); //internal server error
+      }
+
+      utilisateur.id = retour.insertId;
+      resultat.json(utilisateur);
+    }
+  );
+});
+
+app.post("/connexion", (requete, resultat) => {
+  connection.query(
+    "SELECT * FROM utilisateur WHERE email = ?",
+    [requete.body.email],
+    (erreur, lignes) => {
+      if (erreur) {
+        console.error(erreur);
+        return resultat.sendStatus(500); //internal server error
+      }
+
+      //si l'email est inexistant
+      if (lignes.length === 0) {
+        return resultat.sendStatus(401);
+      }
+
+      const motDePasseFormulaire = requete.body.password;
+      const motDePasseHashBaseDeDonnees = lignes[0].password;
+
+      const compatible = bcrypt.compareSync(
+        motDePasseFormulaire,
+        motDePasseHashBaseDeDonnees
+      );
+
+      if (!compatible) {
+        return resultat.sendStatus(401);
+      }
+
+      return resultat.send(
+        jwtUtils.sign({ sub: requete.body.email }, "azerty123")
+      );
+    }
+  );
 });
 
 app.listen(5000, () => console.log("Le serveur écoute sur le port 5000 !!"));
